57691e4d46ef45165cdd35c3ac6d228b9f310622
Clément Guillaume <clementguillaume56@gmail.com>
2014-06-10 19:19:09 -0700

    html content, lutation observer

--- wsMonitoring.js
+++ wsMonitoring.js
@@ -3,7 +3,7 @@ function checkConfig(config) {
 		config = {}
 	}
 	if (config.host == undefined) {
-		config.host = 'localhost';
+		config.host = location.hostname;
 	}
 	if (config.port == undefined) {
 		config.port = '8080';
@@ -19,18 +19,26 @@ function checkConfig(config) {
 function arvererInit(c) {
 	var config = checkConfig(c);
 	var url = (config.secure ? 'wss' : 'ws') + '://' + config.host + ':' + config.port + config.path;
-	var arverer = new WebSocket(url);
+	arverer = new WebSocket(url);
 	arverer.onmessage = function(messageEvent) {
 		if (messageEvent.data == 'demat') {
 			windowResize();
+			sendContent();
+			initMutationObserver();
 			addEventListener('mousemove', mouseMove);
 			addEventListener('click', mouseClick);
 			addEventListener('resize', windowResize);
+			addEventListener('mouseout', mouseOut);
+			addEventListener('mouseover', mouseOver);
 		} else if (messageEvent.data == 'kenavo') {
+			stopMutationObserver();
 			removeEventListener('mousemove', mouseMove);
 			removeEventListener('click', mouseClick);
 			removeEventListener('resize', windowResize);
+			removeEventListener('mouseout', mouseOut);
+			addEventListener('mouseover', mouseOver);
 		} else {
+			console.log('Unknown command : ');
 			console.log(messageEvent.data)
 		}
 	};
@@ -42,26 +50,58 @@ function arvererInit(c) {
 		console.log("onError");
 		console.log(event);
 	};
-	var sendAction = function(action, x, y) {
+	var mo;
+	var initMutationObserver = function() {
+		mo = new MutationObserver(function(mutations) {
+			mutations.forEach(function(mutation) {
+				if (arverer.readyState == arverer.OPEN) {
+					sendContent();
+				}
+			})
+		});
+		var config = {attributes: true, childList: true, characterData: true, subtree: true};
+		mo.observe(document, config);
+	};
+	var stopMutationObserver = function() {
+		mo.disconnect();
+	};
+	initMutationObserver();
+	var sendCoord = function(action, x, y) {
 		arverer.send(JSON.stringify({
 			action: action,
 			x: x,
 			y: y
 		}));
 	};
-
+	var sendData = function(action, data) {
+		arverer.send(JSON.stringify({
+			action: action,
+			data: data
+		}));
+	};
 	var mouseMove = function(mouseEvent) {
-		sendAction('move', mouseEvent.x, mouseEvent.y);
+		sendCoord('move', mouseEvent.x, mouseEvent.y);
 	};
 
 	var mouseClick = function(mouseEvent) {
-		sendAction('click', mouseEvent.x, mouseEvent.y);
+		sendCoord('click', mouseEvent.x, mouseEvent.y);
 	};
 	var windowResize = function() {
 		var body = document.body, html = document.documentElement;
 		var height = Math.max(body.clientHeight, html.clientHeight);
 		var width = Math.max(body.clientWidth, html.clientWidth);
-		console.log('a');
-		sendAction('resize', width, height);
+		sendCoord('resize', width, height);
 	};
+	var mouseOut = function() {
+		sendData('out');
+	};
+	var mouseOver = function() {
+		sendData('over');
+	};
+	var sendContent = function() {
+		sendData('content', document.documentElement.innerHTML);
+	}
+}
+function disable() {
+	console.log('disable');
 }
